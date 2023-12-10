d607f4610fd4d4bbaa02df7c944d99cdb82cab0c
Canaga81 <Canagamemmedov@gmail.com>
2023-12-10 18:15:42 +0400

    Header Start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './Companents/Header'
+
+function App() {
+
+  return (
+    <div className="container mx-auto p-4">
+      <Header />
+    </div>
+  )
+}
+
+export default App
